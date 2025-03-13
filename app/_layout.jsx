4ba5e18536dc1683e62e8043bcd27aa89d2d5a83
Raafia Fatima <raafiafatima5@gmail.com>
2025-03-14 04:14:44 +0500

    Starting project

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,27 @@
-import { Stack } from "expo-router";
+import { Stack, SplashScreen } from "expo-router";
 import React from "react";
 import "../global.css";
+import {useFonts} from 'expo-font'
+import { useState , useEffect } from 'react'
 
 const _layout = () => {
+
+    SplashScreen.preventAutoHideAsync();
+    const [fontsLoaded, error] = useFonts({
+      "Barrio-Regular": require("../assets/fonts/Barrio-Regular.ttf"),
+    });
+  
+    useEffect(() => {
+      if (error) throw error;
+  
+      if (fontsLoaded) {
+        SplashScreen.hideAsync();
+      }
+    }, [fontsLoaded, error]);
+  
+    if (!fontsLoaded && !error) {
+      return null;
+    }
   return (
     <Stack>
       <Stack.Screen
--- app/index.jsx
+++ app/index.jsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function index () {
     return (
-      <View className="flex-1 justify-center items-center bg-black">
-        <Text className="text-white text-2xl font-bold"> textInComponent </Text>
+      <View className="flex-1 justify-center items-center bg-grey-600">
+        <Text className="text-black text-2xl font-bold" style={{ fontFamily: 'barrio' }}> textInComponent </Text>
       </View>
     )
   }
