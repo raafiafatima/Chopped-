d69e1483ac485f28f710d7bd0a7191c79e3d0b7c
Raafia Fatima <raafiafatima5@gmail.com>
2025-03-14 03:59:23 +0500

    Starting project

--- app/index.jsx
+++ app/index.jsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function index () {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View className="flex-1 justify-center items-center bg-black">
+        <Text className="text-white text-2xl font-bold"> textInComponent </Text>
       </View>
     )
   }
